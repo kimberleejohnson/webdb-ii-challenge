d6006a052318008612c35b072fedf77fba1cb33d
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-04 12:26:43 -0700

    Configured knex and updated README

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+//1. Imported knex and sqlite3 in terminal 
+
+//2. Requiring knex
+const knex = require('knex'); 
+
 const express = require('express');
 const helmet = require('helmet');
 
 const server = express();
+
+//3. Configuring knex 
+const knexConfig = {
+  client: 'sqlite3', 
+  connection: {
+    filename: './data/lambda.db3'
+  }, 
+  useNullasDefault: true, 
+}
+
+//4. Defining database 
+const db = knex(knexConfig); 
 
 server.use(express.json());
 server.use(helmet());

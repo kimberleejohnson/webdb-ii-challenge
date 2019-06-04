618b755b6cc0596087f1320741a82482e13e4b82
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-04 12:52:30 -0700

    Refactored to use routers

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-//1. Imported knex and sqlite3 in terminal 
-
-//2. Requiring knex
-const knex = require('knex'); 
-
-const express = require('express');
-const helmet = require('helmet');
-
-const server = express();
-
-//3. Configuring knex 
-const knexConfig = {
-  client: 'sqlite3', 
-  connection: {
-    filename: './data/lambda.db3'
-  }, 
-  useNullasDefault: true, 
-}
-
-//4. Defining database 
-const db = knex(knexConfig); 
-
-server.use(express.json());
-server.use(helmet());
-
-// endpoints here
+const server = require('./api/server.js'); 
 
 const port = 3300;
 server.listen(port, function() {
---
+++ zoos/zoos-router.js
@@ -0,0 +1,35 @@
+const knex = require('knex'); 
+const router = require('express').Router(); 
+
+// To import data (for model later)
+// const Roles = require('./roles/roles-model'); 
+
+// 1. npm install knex and driver 
+// 2. configure knex, after importing up above, and get a connection to database
+
+//3. Configuring knex 
+const knexConfig = {
+    client: 'sqlite3', 
+    connection: {
+      filename: './data/lambda.db3'
+    }, 
+    useNullAsDefault: true, 
+  }
+
+//4. Defining database 
+const db = knex(knexConfig); 
+
+// Endpoints here
+// GET all (R IN CRUD)
+router.get('/', (req, res) => {
+    db('zoos').then(zoos => {
+      res.status(200).json(zoos); 
+    })
+    .catch(error => {
+      res.status(500).json(error); 
+    })
+  }); 
+  
+  // GET by id(R IN CRUD)
+
+  module.exports = router; 
